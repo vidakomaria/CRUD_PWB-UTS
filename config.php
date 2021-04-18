5ea3d6cf2624e047776d05a6c3608b8f07c3a814
vidakomaria <vidakomaria47@gmail.com>
2021-04-18 23:57:09 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 
 try {
-    $connection = new PDO("mysql:host=localhost;dbname=uts192410101093", "root", "");
+    $connection = new PDO("mysql:host=localhost;dbname=uts192410101093", "192410101093", "secret");
 } catch (PDOException $exception) {
     print("DB Error dengan kode" . $exception->getMessage());
 }
+?>
